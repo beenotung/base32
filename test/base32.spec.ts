07a33f4d0f93bbc3aa065d457f3a4e699d531d10
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-01-18 09:28:37 +0800

    remove debug log

--- test/base32.spec.ts
+++ test/base32.spec.ts
@@ -10,6 +10,5 @@ it('e2e test', () => {
   let string = binary_string_to_string(binary_string)
   let bytes = string_to_bytes(string)
   let res = bytes_to_binary_string(bytes)
-  console.log(res)
   expect(res).to.equals('apple')
 })
